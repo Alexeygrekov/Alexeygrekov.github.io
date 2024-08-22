ecad581a20d37b02c3b73acbc785dbf8148b09ca
Alexeygrekov <94085810+Alexeygrekov@users.noreply.github.com>
2024-08-21 19:16:52 -0600

    Changed the height of where the animaation starts

--- js/scripts.js
+++ js/scripts.js
@@ -21,7 +21,7 @@ let scaleFactor = window.innerWidth > 768 ? 1 : 0.75;
   space.add({
     start: (bound) => {
       bound.width = window.innerWidth;
-      bound.height = window.innerHeight;
+      bound.height = window.innerHeight - mobile_navbar_padding;
       // Resize will call the resize function below, drawing the points and
       // rotating line.
       space.resize(bound)
